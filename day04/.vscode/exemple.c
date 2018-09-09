481eae5f2c30a982f2bb2abbd1044a2621980a13
feudal <calmis87@gmail.com>
2018-09-09 16:10:08 +0300

    ex02

---
+++ day04/.vscode/exemple.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    n = ft_iterative_factorial(5);
+    printf("%d\n", n);
+    return(0);
+}
--- day04/ex02/ft_iterative_power.c
+++ day04/ex02/ft_iterative_power.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
+int ft_iterative_power(int nb, int power)
+{
+    int rez;
+
+    rez = 1;
+
+    if (power < 0)
+        return (0);
+    if (power == 0)
+        return(1);
+    while(power >= 1)
+    {
+        rez = rez * nb;
+        power--;
+    }
+    return(rez);
+}
+
 int main()
 {
     int n;
 
-    n = ft_iterative_factorial(5);
+    n = ft_iterative_power(2, 3);
     printf("%d\n", n);
     return(0);
 }
